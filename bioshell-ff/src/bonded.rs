15b07c3e914bedddb363aecc4a5f93ae81f43cf6
Dominik Gront <dgront@chem.uw.edu.pl>
2022-06-01 15:35:37 +0200

    [ff] Spring energy evaluation moved into a macro
    ... to avoid code copy & paste and fix bugs

--- bioshell-ff/src/bonded.rs
+++ bioshell-ff/src/bonded.rs
@@ -15,14 +15,20 @@ impl SimpleHarmonic {
     }
 }
 
+macro_rules! spring_kernel {
+    ($system:expr, $i:expr, $j:expr, $d0:expr, $en:expr, $OP:tt) => {
+        let v:f64 = ($system.distance_square($i, $j).sqrt() - $d0) as f64;
+        $en $OP v*v;
+    }
+}
+
 impl Energy for SimpleHarmonic {
 
     fn energy(&self, system: &Coordinates) -> f64 {
         let mut en:f64 = 0.0;
         for i in 1..system.size() {
             if system[i].chain_id == system[i-1].chain_id {
-                let mut v:f64 = (system.distance_square(i, i - 1).sqrt() - self.d0) as f64;
-                en += v*v;
+                spring_kernel!(system,i, i-1, self.d0, en, +=);
             }
         }
 
@@ -32,10 +38,10 @@ impl Energy for SimpleHarmonic {
     fn energy_by_pos(&self, system: &Coordinates, pos: usize) -> f64 {
         let mut en: f64 = 0.0;
         if pos > 0 && system[pos].chain_id == system[pos - 1].chain_id {
-            en += (system.distance_square(pos, pos - 1).sqrt() - self.d0) as f64;
+            spring_kernel!(system,pos, pos-1,self.d0, en, +=);
         }
         if pos < system.size() - 1 && system[pos].chain_id == system[pos + 1].chain_id {
-            en += (system.distance_square(pos, pos + 1).sqrt() - self.d0) as f64;
+            spring_kernel!(system,pos, pos-1,self.d0, en, +=);
         }
 
         return en * self.k as f64;
@@ -43,6 +49,16 @@ impl Energy for SimpleHarmonic {
 
     fn delta_energy_by_range(&self, old: &Coordinates, pos: &Range<usize>, new: &Coordinates) -> f64 {
 
-        return 0.0;
+        let mut en: f64 = 0.0;
+        let start: usize = if pos.start > 0 { pos.start - 1 } else { pos.start };
+        let end: usize = if pos.end >= old.size() - 1 { old.size() - 1 } else { pos.end + 1 };
+        for ipos in start..end {
+            if old[ipos].chain_id == old[ipos + 1].chain_id {
+                spring_kernel!(new, ipos, ipos+1, self.d0, en, +=);
+                spring_kernel!(old, ipos, ipos+1, self.d0, en, -=);
+            }
+        }
+
+        return en * self.k as f64;
     }
 }
