464b7d810428c875c912b801327a895d41473d41
Dominik Gront <dgront@chem.uw.edu.pl>
2023-01-06 23:09:03 +0100

    Refactoring of simulation engine.
    Obsolete code removed

--- bioshell-ff/src/ff.rs
+++ bioshell-ff/src/ff.rs
@@ -2,28 +2,6 @@ use std::ops::Range;
 
 use bioshell_sim::{Energy};
 
-// pub trait Energy {
-//     fn energy(&self, system: &System) -> f64;
-//     fn energy_by_pos(&self, system: &System, pos: usize) -> f64;
-//     fn delta_energy_by_range(&self, old: &System, pos: &Range<usize>, new: &System) -> f64;
-//     /// Returns the name of this energy function
-//     /// The returned name may be used to identify this energy, e.g. to name a column in a score table
-//     fn name(&self) -> String;
-// }
-
-// pub struct ZeroEnergy{}
-//
-// impl Energy for ZeroEnergy {
-//     #[allow(unused)]
-//     fn energy(&self, system: &System) -> f64 { 0.0 }
-//     #[allow(unused)]
-//     fn energy_by_pos(&self, system: &System, pos: usize) -> f64 { 0.0 }
-//     #[allow(unused)]
-//     fn delta_energy_by_range(&self, old: &System, moved: &Range<usize>, new: &System) -> f64 { 0.0 }
-//     #[allow(unused)]
-//     fn name(&self) -> String { String::from("ZeroEnergy") }
-// }
-
 /// Stores energy components and their weights
 /// Allows one calculate the total energy methods for a given Coordinates
 pub struct TotalEnergy<S> {
