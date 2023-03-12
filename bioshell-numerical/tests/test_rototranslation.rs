d9d678fda1a48c89b843cfaba1b82d230765747b
Dominik Gront <dgront@chem.uw.edu.pl>
2023-03-12 22:16:38 +0100

    [numerical] fixing test_rototranslation.rs to have it compile

--- bioshell-numerical/tests/test_rototranslation.rs
+++ bioshell-numerical/tests/test_rototranslation.rs
@@ -1,16 +1,15 @@
 extern crate bioshell_numerical;
 
 use bioshell_numerical::{Vec3, Rototranslation};
-use bioshell_numerical::RotoTranslation;
 
 #[test]
 fn rototranslation_from_column_vectors() {
-    let cx: Vec3 = Vec3(1,2,3);
-    let cy: Vec3 = Vec3(1,2,3);
-    let cz: Vec3 = Vec3(1,2,3);
-    let center: Vec3 = Vec3(1,2,3);
+    let cx: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+    let cy: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+    let cz: Vec3 = Vec3::new(1.0, 2.0, 3.0);
+    let center: Vec3 = Vec3::new(1.0, 2.0, 3.0);
 
-    let rotot:Rototranslation = Rototranslation.from_column_vectors(cx, cy, cz, center);
+    // let rotot:Rototranslation = Rototranslation::from_column_vectors(cx, cy, cz, center);
 
-    assert_eq!()
+    // assert_eq!()
 }
