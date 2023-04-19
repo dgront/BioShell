542cbaa68202a85408793f591d1cb675f0ff2c59
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-04-19 10:52:16 +0200

    Rototranslation verified

--- bioshell-numerical/tests/test_rototranslation.rs
+++ bioshell-numerical/tests/test_rototranslation.rs
@@ -13,17 +13,17 @@ mod rototranslation_test {
         let _origin_vector: Vec3 = Vec3::new(1.0, 1.0, 1.0);
         let _start_vector: Vec3 = Vec3::new(0.0, 0.0, 0.0);
         let _end_vector: Vec3 = Vec3::new(4.0, 4.0, 4.0);
-        let _theta_rad = 360.0 * std::f64::consts::PI /180.0;
+        let _theta_rad = 180.0 * std::f64::consts::PI /180.0;
 
         let _roto = Rototranslation::around_axis(_origin_vector, _start_vector, _end_vector, _theta_rad);
 
-        let _candidate_vector: Vec3 = Vec3::new(3.0, 3.0, 3.0);
+        let _candidate_vector: Vec3 = Vec3::new(1.0, 2.0, 3.0);
 
         let _rotated_vec = _roto.apply_mut(&_candidate_vector);
 
         assert_eq!(truncate(_rotated_vec.x), 3.0);
-        assert_eq!(truncate(_rotated_vec.y), 3.0);
-        assert_eq!(truncate(_rotated_vec.z), 3.0);
+        assert_eq!(truncate(_rotated_vec.y), 2.0);
+        assert_eq!(truncate(_rotated_vec.z), 1.0);
     }
 
     #[test]
@@ -31,16 +31,16 @@ mod rototranslation_test {
         let _origin_vector: Vec3 = Vec3::new(1.0, 1.0, 1.0);
         let _start_vector: Vec3 = Vec3::new(0.0, 0.0, 0.0);
         let _end_vector: Vec3 = Vec3::new(4.0, 4.0, 4.0);
-        let _theta_rad = 360.0 * std::f64::consts::PI /180.0;
+        let _theta_rad = 180.0 * std::f64::consts::PI /180.0;
 
         let _roto = Rototranslation::around_axis(_origin_vector, _start_vector, _end_vector, _theta_rad);
 
-        let mut _candidate_vector: Vec3 = Vec3::new(3.0, 3.0, 3.0);
+        let mut _candidate_vector: Vec3 = Vec3::new(1.0, 2.0, 3.0);
 
         let _rotated_vec = _roto.apply_inverse_mut(&mut _candidate_vector);
 
-        assert_eq!(_rotated_vec.x, 3.0);
-        assert_eq!(_rotated_vec.y, 3.0);
-        assert_eq!(_rotated_vec.z, 3.0);
+        assert_eq!(truncate(_rotated_vec.x), 3.0);
+        assert_eq!(truncate(_rotated_vec.y), 2.0);
+        assert_eq!(truncate(_rotated_vec.z), 1.0);
     }
 }
