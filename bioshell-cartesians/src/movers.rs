2ae1dfd36ebd4ce72ff3cb743020e6c144b79b82
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-05-20 19:00:29 +0200

    [TerminalMove] done.

--- bioshell-cartesians/src/movers.rs
+++ bioshell-cartesians/src/movers.rs
@@ -3,9 +3,9 @@ use std::ops::Range;
 use crate::CartesianSystem;
 use bioshell_montecarlo::{AcceptanceCriterion, AcceptanceStatistics, Mover};
 use bioshell_sim::{Energy, System};
-//use bioshell_numerical::matrix::Matrix3x3;
 use bioshell_numerical::vec3::Vec3;
-use bioshell_numerical::Rototranslation;
+use bioshell_numerical::type_casting;
+use bioshell_numerical::{random_point_nearby, Rototranslation};
 
 /// A mover that moves a single, randomly selected atom by a small random vector.
 pub struct SingleAtomMove {
@@ -74,92 +74,6 @@ impl<E: Energy<CartesianSystem>> Mover<CartesianSystem, E> for SingleAtomMove {
 
     fn set_max_range(&mut self, new_val: f64) {
         self.max_step = new_val;
-    }
-}
-
-
-
-/// A mover that applies a crankshaft move to a molecule.
-pub struct CrankshaftMove {
-    max_angle: f64,
-    frag_size: usize,//how far would be the axis positions
-    succ_rate: AcceptanceStatistics,
-}
-
-impl CrankshaftMove {
-    /// Create a new mover that applies a crankshaft move.
-    pub fn new(max_angle: f64) -> CrankshaftMove {
-        CrankshaftMove {
-            max_angle,
-            frag_size:5,
-            succ_rate: Default::default(),
-        }
-    }
-}
-
-impl<E: Energy<CartesianSystem>> Mover<CartesianSystem, E> for CrankshaftMove {
-    fn perturb(
-        &mut self,
-        system: &mut CartesianSystem,
-        energy: &E,
-        acc: &mut dyn AcceptanceCriterion,
-    ) -> Option<Range<usize>>
-    {
-        let mut rng = rand::thread_rng();//obtain a random number generator
-        let system_length = system.size();//obtain the length of the Cartesian system
-        let i_start = rng.gen_range(0..system_length - &self.frag_size-1);//obtain a random start position
-        let mut i_end = i_start + &self.frag_size + 1;//obtain the end position relative to the start position
-        let angle = rng.gen_range(-&self.max_angle..self.max_angle);//obtain a random angle
-        ////let mut system_coords = system.coordinates();//returns an immutable copy of the coordinates
-        let mut start = system.coordinates()[i_start].clone();
-        let mut end = system.coordinates()[i_end].clone();
-        let roto_tran = Rototranslation::around_axis(&start, &end, angle);
-
-        let energy_before = energy.energy(system);//calculate the energy before the move
-
-        //apply forward rotation
-        for i in i_start +1..i_end  {
-            let mut temp_coord:Vec3 = system.coordinates()[i].clone();
-            //print!("Before ({},{},{})", temp_coord.x, temp_coord.y, temp_coord.z);
-            roto_tran.apply_mut(&mut temp_coord);
-            //println!("After ({},{},{})", temp_coord.x, temp_coord.y, temp_coord.z);
-            system.set_vec(i, temp_coord);
-        }
-
-        let energy_after = energy.energy(system);
-
-        //println!("before={}, after={}", energy_before, energy_after);
-
-        if acc.check(energy_before, energy_after)
-        {
-            //if move succeeds
-            self.succ_rate.n_succ += 1;
-            return Option::from(i_start..i_end + 1);
-        }
-        else
-        {
-            //if move doesn't succeed
-            self.succ_rate.n_failed += 1;
-            //fall back - apply inverse rotation
-            for i in i_start +1..i_end  {
-                let mut temp_coord:Vec3 = system.coordinates()[i].clone();
-                roto_tran.apply_inverse_mut(&mut temp_coord);
-                system.set_vec(i, temp_coord);
-            }
-            return None;
-        }
-    }
-
-    fn acceptance_statistics(&self) -> AcceptanceStatistics {
-        self.succ_rate.clone()
-    }
-
-    fn max_range(&self) -> f64 {
-        self.max_angle
-    }
-
-    fn set_max_range(&mut self, new_val: f64) {
-        self.max_angle = new_val;
     }
 }
 
@@ -246,46 +160,108 @@ impl<E: Energy<CartesianSystem>> Mover<CartesianSystem, E> for ChangeVolume {
     }
 }
 
-/// A mover that applies a terminal move to a molecule.
-pub struct TerminalMove {
+/// A mover that applies a crankshaft move to a molecule.
+pub struct CrankshaftMove {
     max_angle: f64,
+    frag_size: usize,//how far would be the axis positions
     succ_rate: AcceptanceStatistics,
 }
 
-// A class named to perform a random rotation at the termini around a random axis passing through
-// the Cα atom. The move involves a rigid-body rotation of one or two peptide bonds at one of the
-// chain termini picked randomly with rotation axis passing through the alpha carbon in a random
-// direction. It is important that in this scheme any rotation and its inverse are picked with
-// equal probability.
+impl CrankshaftMove {
+    /// Create a new mover that applies a crankshaft move.
+    pub fn new(max_angle: f64) -> CrankshaftMove {
+        CrankshaftMove {
+            max_angle,
+            frag_size:5,
+            succ_rate: Default::default(),
+        }
+    }
+}
+
+impl<E: Energy<CartesianSystem>> Mover<CartesianSystem, E> for CrankshaftMove {
+    fn perturb(
+        &mut self,
+        system: &mut CartesianSystem,
+        energy: &E,
+        acc: &mut dyn AcceptanceCriterion,
+    ) -> Option<Range<usize>>
+    {
+        let mut rng = rand::thread_rng();//obtain a random number generator
+        let system_length = system.size();//obtain the length of the Cartesian system
+        let i_start = rng.gen_range(0..system_length - &self.frag_size-1);//obtain a random start position
+        let mut i_end = i_start + &self.frag_size + 1;//obtain the end position relative to the start position
+        let angle = rng.gen_range(-&self.max_angle..self.max_angle);//obtain a random angle
+        let mut start = system.coordinates()[i_start].clone();
+        let mut end = system.coordinates()[i_end].clone();
+        let roto_tran = Rototranslation::around_axis(&start, &end, angle);
+
+        let energy_before = energy.energy(system);//calculate the energy before the move
+
+        //apply forward rotation
+        for i in i_start +1..i_end  {
+            let mut temp_coord:Vec3 = system.coordinates()[i].clone();
+            //print!("Before ({},{},{})", temp_coord.x, temp_coord.y, temp_coord.z);
+            roto_tran.apply_mut(&mut temp_coord);
+            //println!("After ({},{},{})", temp_coord.x, temp_coord.y, temp_coord.z);
+            system.set_vec(i, temp_coord);
+        }
+
+        let energy_after = energy.energy(system);
+
+        if acc.check(energy_before, energy_after)
+        {
+            //if move succeeds
+            self.succ_rate.n_succ += 1;
+            return Option::from(i_start..i_end + 1);
+        }
+        else
+        {
+            //if move doesn't succeed
+            self.succ_rate.n_failed += 1;
+            //fall back - apply inverse rotation
+            for i in i_start +1..i_end  {
+                let mut temp_coord:Vec3 = system.coordinates()[i].clone();
+                roto_tran.apply_inverse_mut(&mut temp_coord);
+                system.set_vec(i, temp_coord);
+            }
+            return None;
+        }
+    }
+
+    fn acceptance_statistics(&self) -> AcceptanceStatistics {
+        self.succ_rate.clone()
+    }
+
+    fn max_range(&self) -> f64 {
+        self.max_angle
+    }
+
+    fn set_max_range(&mut self, new_val: f64) {
+        self.max_angle = new_val;
+    }
+}
+
+
+
+/// A mover that applies a terminal move to a molecule.
+pub struct TerminalMove {
+    max_angle: f64,
+    frag_size: usize,
+    succ_rate: AcceptanceStatistics,
+}
+
+// The move involves a rotation of one or two peptide bonds at one of the chain termini picked
+// randomly with rotation axis passing through the alpha carbon in a random direction.
+// It is important that in this scheme any rotation and its inverse are picked with equal
+// probability.
 impl TerminalMove {
     /// Create a new mover that applies a terminal move.
     pub fn new(max_angle: f64) -> TerminalMove {
         TerminalMove {
             max_angle,
+            frag_size:5,
             succ_rate: Default::default(),
         }
-    }
-
-    fn apply(&self, system: &mut CartesianSystem, i1: usize, i2: usize, angle: f64) {
-        let mut rng = rand::thread_rng();
-
-        // Generate a random direction for the rotation axis passing through the Cα atom
-        let mut axis = Vec3::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0));
-        axis.normalize();
-
-        let v1 = system.coordinates()[i1].clone();
-        let v2 = system.coordinates()[i2].clone();
-        let end = v2 - v1;
-
-        let rt = Rototranslation::around_axis(&v1, &axis, angle);
-
-        let mut v3 = rt.apply(&end);
-        v3 = v3 - end;
-
-        let coords = system.coordinates();
-        //for i in (i1 + 1)..i2 {
-        //    rt.apply_mut(&mut coords[i]);
-        //}
     }
 }
 
@@ -296,27 +272,58 @@ impl<E: Energy<CartesianSystem>> Mover<CartesianSystem, E> for TerminalMove {
         energy: &E,
         acc: &mut dyn AcceptanceCriterion,
     ) -> Option<Range<usize>> {
-        let mut rng = rand::thread_rng();//obtain a random number generator
-        let size = system.size();//obtain the number of atoms in the Cartesian system
+        let mut rng = rand::thread_rng();
+        let size = system.size();
+        let terminal_flag = rng.gen_range(0..1 + 1);
 
-        let i1 = rng.gen_range(0..(size - 2));//obtain a random value from (0) to (size-2)
-        let i2 = i1 + 1;
+        let mut i_start = 0;
+        let mut i_end = 0;
+        let mut terminal_vec = Vec3::zero();
+        let mut term_rot_axis_start = Vec3::zero();
 
-        let old_en = energy.energy(system);//obtain the total energy of the Cartesian system
-        let angle = rng.gen_range(-&self.max_angle..self.max_angle);//obtain a random angle
-        self.apply(system, i1, i2, angle);
-        let new_en = energy.energy(system);//obtain the total energy of the system again
-
-
-
-        if acc.check(old_en, new_en)
-        {
-            self.succ_rate.n_succ += 1;
-            return Option::from(i1..i2 + 1);
+        if terminal_flag == 0 {
+            i_start = 0;
+            i_end = self.frag_size;
+            terminal_vec = system.coordinates()[i_start];
+            term_rot_axis_start = system.coordinates()[i_end];
+        } else {
+            i_start = size - 1 - self.frag_size;
+            i_end = size - 1;
+            term_rot_axis_start = system.coordinates()[i_start];
+            terminal_vec = system.coordinates()[i_end];
         }
-        else
+
+        let angle = rng.gen_range(-&self.max_angle..self.max_angle);
+        let start = term_rot_axis_start;
+        let end = random_point_nearby(&start, self.frag_size as f64);
+        let roto_tran = Rototranslation::around_axis(&start, &end, angle);
+        let energy_before = energy.energy(system);//calculate the energy before the move
+
+        //apply forward rotation
+        for i in i_start + 1..i_end {
+            let mut temp_coord: Vec3 = system.coordinates()[i].clone();
+            //print!("Before ({},{},{})", temp_coord.x, temp_coord.y, temp_coord.z);
+            roto_tran.apply_mut(&mut temp_coord);
+            //println!("After ({},{},{})", temp_coord.x, temp_coord.y, temp_coord.z);
+            system.set_vec(i, temp_coord);
+        }
+
+        let energy_after = energy.energy(system);
+
+        if acc.check(energy_before, energy_after)
         {
-            self.apply(system, i1, i2, -angle);
+            //if move succeeds
+            self.succ_rate.n_succ += 1;
+            return Option::from(i_start..i_end + 1);
+        } else {
+            //if move doesn't succeed
+            self.succ_rate.n_failed += 1;
+            //fall back - apply inverse rotation
+            for i in i_start + 1..i_end {
+                let mut temp_coord: Vec3 = system.coordinates()[i].clone();
+                roto_tran.apply_inverse_mut(&mut temp_coord);
+                system.set_vec(i, temp_coord);
+            }
             return None;
         }
     }
--- bioshell-numerical/src/lib.rs
+++ bioshell-numerical/src/lib.rs
@@ -8,5 +8,7 @@ pub use rototranslation::*;
 pub use vec3::*;
 
 mod testing_macros;
+pub mod type_casting;
+
 pub use testing_macros::*;
 
