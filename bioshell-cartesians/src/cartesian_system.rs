313b7b2c50a5c2d5030c6f297aa157c63b4ddc7f
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-05-30 20:32:24 +0200

    [CartesianSystem] get_periodic_image() implemented.

--- bioshell-cartesians/src/cartesian_system.rs
+++ bioshell-cartesians/src/cartesian_system.rs
@@ -157,6 +157,67 @@ impl CartesianSystem {
         };
         self.neighbor_list.update_all_for_view(stls_v);
     }
+
+    /// Calculates the periodic image of a vector relative to a reference point
+    /*pub fn periodic_image(&self, reference: &Vec3, vector: &Vec3) -> Vec3 {
+        let box_len = self.box_len();
+
+        let mut image = vector.clone();
+        let mut delta = reference.clone() - *vector;
+
+        // Apply periodic boundary conditions along each dimension
+        if delta.x > 0.5 * box_len {
+            delta.x -= box_len;
+            image.x += box_len;
+        } else if delta.x < -0.5 * box_len {
+            delta.x += box_len;
+            image.x -= box_len;
+        }
+
+        if delta.y > 0.5 * box_len {
+            delta.y -= box_len;
+            image.y += box_len;
+        } else if delta.y < -0.5 * box_len {
+            delta.y += box_len;
+            image.y -= box_len;
+        }
+
+        if delta.z > 0.5 * box_len {
+            delta.z -= box_len;
+            image.z += box_len;
+        } else if delta.z < -0.5 * box_len {
+            delta.z += box_len;
+            image.z -= box_len;
+        }
+
+        image
+    }*/
+    /// Calculates the periodic image of a vector relative to a reference point
+    pub fn get_periodic_image(&self, reference: &Vec3, vector: &Vec3) -> Vec3 {
+        let box_len = self.box_len();
+
+        let mut image = *vector;
+        let delta = *reference - *vector;
+
+        // Apply periodic boundary conditions along each dimension
+        let half_box_len = 0.5 * box_len;
+
+        // X-axis
+        if delta.x.abs() > half_box_len {
+            image.x += box_len * delta.x.signum();
+        }
+
+        // Y-axis
+        if delta.y.abs() > half_box_len {
+            image.y += box_len * delta.y.signum();
+        }
+
+        // Z-axis
+        if delta.z.abs() > half_box_len {
+            image.z += box_len * delta.z.signum();
+        }
+        image
+    }
 }
 
 impl System for CartesianSystem {
--- bioshell-cartesians/tests/test_cartesian_system.rs
+++ bioshell-cartesians/tests/test_cartesian_system.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod cartesian_system_test {
-    use bioshell_cartesians::{CartesianSystem, Coordinates, NbList, PolymerRules};
+    use bioshell_cartesians::{CartesianSystem, Coordinates, NbList, NbListRules, PolymerRules};
     use bioshell_numerical::{Rototranslation, Vec3, assert_eq_vec3, assert_eq_float};
 
     #[test]
@@ -9,9 +9,9 @@ mod cartesian_system_test {
         let buffer_thickness = 4.0;
         let nbl: NbList = NbList::new(E_TO, buffer_thickness, Box::new(PolymerRules {}));
         let mut coords = Coordinates::new(3);//create a coordinate of length: 3.
-        coords.add(0, 1.0,1.0,1.0);
-        coords.add(1, 2.0,2.0,2.0);
-        coords.add(2, 3.0,3.0,3.0);
+        coords.add(0, 1.0, 1.0, 1.0);
+        coords.add(1, 2.0, 2.0, 2.0);
+        coords.add(2, 3.0, 3.0, 3.0);
 
         let system: CartesianSystem = CartesianSystem::new(coords, nbl);
 
@@ -26,9 +26,9 @@ mod cartesian_system_test {
         let buffer_thickness = 4.0;
         let nbl: NbList = NbList::new(E_TO, buffer_thickness, Box::new(PolymerRules {}));
         let mut coords = Coordinates::new(3);   //create a coordinate of length: 3.
-        coords.add(0, 1.0,1.0,1.0);
-        coords.add(1, 2.0,2.0,2.0);
-        coords.add(2, 3.0,3.0,3.0);
+        coords.add(0, 1.0, 1.0, 1.0);
+        coords.add(1, 2.0, 2.0, 2.0);
+        coords.add(2, 3.0, 3.0, 3.0);
 
         let mut system: CartesianSystem = CartesianSystem::new(coords, nbl);
 
@@ -51,11 +51,11 @@ mod cartesian_system_test {
         let buffer_thickness = 4.0;
         let nbl: NbList = NbList::new(E_TO, buffer_thickness, Box::new(PolymerRules {}));
         let mut coords = Coordinates::new(5);
-        coords.add(0, 1.0,1.0,1.0);
-        coords.add(1, 1.0,2.0,3.0);
-        coords.add(2, 2.0,3.0,4.0);
-        coords.add(3, 3.0,4.0,5.0);
-        coords.add(4, 5.0,5.0,5.0);
+        coords.add(0, 1.0, 1.0, 1.0);
+        coords.add(1, 1.0, 2.0, 3.0);
+        coords.add(2, 2.0, 3.0, 4.0);
+        coords.add(3, 3.0, 4.0, 5.0);
+        coords.add(4, 5.0, 5.0, 5.0);
         let mut system: CartesianSystem = CartesianSystem::new(coords, nbl);
         assert_eq_vec3!(system.coordinates()[0], Vec3::new(1.0, 1.0, 1.0), 0.00001);
         assert_eq_vec3!(system.coordinates()[1], Vec3::new(1.0, 2.0, 3.0), 0.00001);
@@ -75,5 +75,31 @@ mod cartesian_system_test {
         assert_eq_vec3!(system.coordinates()[1], Vec3::new(3.0, 2.0, 1.0), 0.00001);
         assert_eq_vec3!(system.coordinates()[2], Vec3::new(4.0, 3.0, 2.0), 0.00001);
         assert_eq_vec3!(system.coordinates()[3], Vec3::new(5.0, 4.0, 3.0), 0.00001);
+    }
+
+    #[test]
+    fn cartesian_system_periodic_image_test() {
+        const E_TO: f64 = 6.0;
+        let buffer_thickness = 4.0;
+        let nbl: NbList = NbList::new(E_TO, buffer_thickness, Box::new(PolymerRules {}));
+        let mut coords = Coordinates::new(5);
+        let mut coords = Coordinates::new(5);
+        coords.add(0, 1.0, 1.0, 1.0);
+        coords.add(1, 1.0, 2.0, 3.0);
+        coords.add(2, 2.0, 3.0, 4.0);
+        coords.add(3, 3.0, 4.0, 5.0);
+        coords.add(4, 5.0, 5.0, 5.0);
+        let mut system: CartesianSystem = CartesianSystem::new(coords, nbl);
+        system.set_box_len(10.0);
+
+        // Set the coordinates of a reference point and a vector
+        let reference = Vec3::new(5.0, 5.0, 5.0);
+        let vector = Vec3::new(11.0, -11.0, 0.0);
+
+        // Calculate the periodic image of the vector
+        let image = system.get_periodic_image(&reference, &vector);
+
+        // Assert that the image coordinates are correctly calculated
+        assert_eq!(image, Vec3::new(1.0, -1.0, 0.0));
     }
 }
