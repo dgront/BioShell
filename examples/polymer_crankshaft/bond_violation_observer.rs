33a3cb20c6f923cc766017c276e33ac30356b16e
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-06-10 14:56:52 +0200

    [examples/polymer_crankshaft] BondViolationObserver finished.

--- examples/polymer_crankshaft/bond_violation_observer.rs
+++ examples/polymer_crankshaft/bond_violation_observer.rs
@@ -6,6 +6,7 @@ pub struct BondViolationObserver {
     pub out_fname: String,
     pub if_append: bool,
     i_model: usize,
+    longest_bond_length: f64,
 }
 
 impl BondViolationObserver {
@@ -14,6 +15,7 @@ impl BondViolationObserver {
             out_fname: fname,
             if_append,
             i_model: 0,
+            longest_bond_length: 0.0,
         }
     }
 }
@@ -24,16 +26,28 @@ impl Observer for BondViolationObserver {
     fn observe(&mut self, object: &Self::S) {
         let coords = object.get_coordinates();
         let mut out_writer = out_writer(&self.out_fname, self.if_append);
-        out_writer
-            .write(format!("{:.6} ", self.i_model).as_bytes())
-            .ok();
-        for _ic in 0..coords.get_chains_count() {
-            // HERE !!!
-            let result: f64 = 0.12345;
-            out_writer.write(format!("{:>10.3} ", result).as_bytes()).ok();
+        out_writer.write(format!("{:.6} ", self.i_model).as_bytes()).ok();
+
+        let num_atoms = coords.get_size();
+        let mut longest_bond_length = 0.0;
+
+        for i in 0..num_atoms - 1 {
+            let j = i + 1;
+            let distance_squared = coords.get_closest_distance_square(i, j);
+            let distance = distance_squared.sqrt();
+
+            if distance > longest_bond_length {
+                longest_bond_length = distance;
+            }
         }
+
+        out_writer
+            .write(format!("{:>10.3} ", longest_bond_length).as_bytes())
+            .ok();
+
         out_writer.write("\n".as_bytes()).ok();
         self.i_model += 1;
+        self.longest_bond_length = longest_bond_length;
     }
 
     fn flush(&mut self) {}
@@ -42,7 +56,7 @@ impl Observer for BondViolationObserver {
         "BondViolationObserver"
     }
 
-    fn as_any(&self) -> &dyn Any {
+    fn as_any(&self) -> &dyn std::any::Any {
         self
     }
 }
