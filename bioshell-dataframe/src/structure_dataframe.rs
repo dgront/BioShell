bd9a03eb22f5f8c03107a914870616207e632391
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2023-07-14 02:01:31 +0200

    [bioshell-dataframe] created

---
+++ bioshell-dataframe/src/structure_dataframe.rs
@@ -0,0 +1,11 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::convert::TryFrom;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub struct StructureDataFrame {
+
+}
+
+impl StructureDataFrame {
+}
