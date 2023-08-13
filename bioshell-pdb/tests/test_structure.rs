395f9fa6291a975145d0f8c947f127feffe6d202
Dominik Gront <dgront@chem.uw.edu.pl>
2023-08-13 10:47:20 -0700

    [bioshell-pdb]  a test for the Structure struct.

---
+++ bioshell-pdb/tests/test_structure.rs
@@ -0,0 +1,24 @@
+use bioshell_pdb::{PdbAtom, Structure};
+
+#[allow(non_upper_case_globals)]
+const lines:  [&str;9] = [
+    "ATOM    514  CA  MET A  60      26.532  28.200  28.365  1.00 17.85           N",
+    "ATOM    515  CA  CYS A  61      25.790  28.757  29.513  1.00 16.12           C",
+    "ATOM    516  CA  GLY A  62      26.891  29.054  30.649  1.00 15.28           C",
+    "ATOM    517  CA  ILE A  63      26.657  29.867  31.341  1.00 20.90           O",
+    "ATOM    518  CA  ALA A  64      25.155  27.554  29.987  1.00 21.91           C",
+    "ATOM    514  CA  MET B  61      26.532  28.200  28.365  1.00 17.85           N",
+    "ATOM    515  CA  ALA B  62      25.790  28.757  29.513  1.00 16.12           C",
+    "ATOM    516  CA  CYS B  63      26.891  29.054  30.649  1.00 15.28           C",
+    "ATOM    518  CA  ALW B  64      25.155  27.554  29.987  1.00 21.91           C"];
+
+#[test]
+fn test_backbone_filtering() {
+    let atoms: Vec<PdbAtom> = lines.iter().map(|l| PdbAtom::from_atom_line(l)).collect();
+    let mut strctr = Structure::from_iterator(atoms.iter());
+    let seq_A = strctr.sequence("A");
+    assert_eq!(seq_A.to_string(), "MCGIA");
+
+    let seq_A = strctr.sequence("B");
+    assert_eq!(seq_A.to_string(), "MACX");
+}
