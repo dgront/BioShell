b63edc9b9ae515e2df67b677471efe9c957b3c99
Dominik Gront <dgront@chem.uw.edu.pl>
2023-08-13 10:47:01 -0700

    [bioshell-pdb]  read_pdb.rs is an example that reads a pdb file and prints a sequence for every chain found

---
+++ bioshell-pdb/examples/read_pdb.rs
@@ -0,0 +1,13 @@
+use std::env;
+use bioshell_pdb::{Structure, load_pdb};
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    let mut strctr = load_pdb(&args[1]).unwrap();
+    println!("Chains: {:?}",strctr.chain_ids());
+
+    for chain_id in strctr.chain_ids() {
+
+        println!("{}", strctr.sequence(&chain_id));
+    }
+}
