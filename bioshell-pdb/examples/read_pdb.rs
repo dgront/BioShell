45626dd08559a3ae26dc27a663f8f3cf7721fecc
Dominik Gront <dgront@chem.uw.edu.pl>
2023-11-01 22:45:10 +0100

    [pdb] main.exe app removed (does nothing)

--- bioshell-pdb/examples/read_pdb.rs
+++ bioshell-pdb/examples/read_pdb.rs
@@ -3,7 +3,7 @@ use bioshell_pdb::{load_pdb_file};
 
 fn main(){
     let args: Vec<String> = env::args().collect();
-    let mut strctr = load_pdb_file(&args[1]).unwrap();
+    let strctr = load_pdb_file(&args[1]).unwrap();
     println!("Chains: {:?}",strctr.chain_ids());
 
     for chain_id in strctr.chain_ids() {
