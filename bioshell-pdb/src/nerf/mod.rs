dcefcc2169e00c7c01fff06172dc769f2aa6923b
Dominik Gront <dgront@chem.uw.edu.pl>
2023-11-25 19:23:26 +0100

    [pdb] KinematicAtomTree implemented.
    KinematicAtomTree provides a handy API to build biopolymer chains with the NeRF algorithm. Each atom must be inserted to the tree as InternalAtomDefinition struct

--- bioshell-pdb/src/nerf/mod.rs
+++ bioshell-pdb/src/nerf/mod.rs
@@ -1,4 +1,6 @@
 //! Natural Extension Reference Frame (NeRF) translates from internal to Cartesian coordinates
 mod nerf;
+mod kinematic_atom_tree;
 
 pub use nerf::*;
+pub use kinematic_atom_tree::*;
