0d85452d66ee02b99778e582f4a455a38c9d5177
Dominik Gront <dgront@chem.uw.edu.pl>
2023-12-02 17:40:56 +0100

    [pdb] distance_map.rs example got a simple command line options.
    The distance_map.rs example can compute the distance map either for the full structure, for backbone, or a CA-only

--- bioshell-pdb/examples/distance_map.rs
+++ bioshell-pdb/examples/distance_map.rs
@@ -1,21 +1,43 @@
 use std::env;
+use clap::error::ContextKind::Usage;
 use bioshell_pdb::{load_pdb_file, PdbAtom};
 use bioshell_pdb::calc::distance;
-use bioshell_pdb::pdb_atom_filters::{IsBackbone, IsCA, PdbAtomPredicate};
+use bioshell_pdb::pdb_atom_filters::{AlwaysPass, IsBackbone, IsCA, PdbAtomPredicate};
 
 fn atom_descr(a: &PdbAtom) -> String {
     format!("{} {} {:4} {}",a.chain_id, a.res_name, a.res_seq, a.name)
 }
 
+const USAGE: &str = "Simple program to compute a matrix of inter-atomic distances for a given PDB file
+
+    distance_map [--ca|--bb] input.pdb
+";
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut strctr = load_pdb_file(&args[1]).unwrap();
-    strctr.drop_ligands();
+    let mut selector: Box<dyn PdbAtomPredicate> = Box::new(AlwaysPass);
+    let mut fname = "";
+    match args.len() {
+        1 => { panic!("No input PDB file given") }
+        2 => { fname = &args[1] }
+        3 => {
+            match args[1].as_str() {
+                "--ca" => { selector = Box::new(IsCA); }
+                "--bb" => { selector = Box::new(IsBackbone); }
+                _ => { panic!("Unknown PDB atom predicate; run `distance_map -h` for help")}
+            }
+            fname = &args[2];
+        }
+        _ => { panic!("Too many arguments; run `distance_map -h` for help") }
+    };
+    if fname == "--help" || fname == "-h" {
+        println!("{:?}", Usage);
+        return;
+    }
+    let mut strctr = load_pdb_file(&fname).unwrap();
 
-    // let selector = IsCA;
-    let selector = IsBackbone;
     for ai in strctr.atoms().iter().filter(|&a|selector.check(a)) {
         for aj in strctr.atoms().iter().filter(|&a|selector.check(a)) {
+            if ai.res_seq == aj.res_seq && ai.i_code==aj.i_code { break }
             println!("{} {} : {}", atom_descr(ai), atom_descr(aj), distance(ai, aj));
         }
     }
