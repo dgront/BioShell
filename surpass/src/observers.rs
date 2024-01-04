151d692012fad83b206c23aa91d2ca13abc5b2fd
Dominik Gront <dgront@chem.uw.edu.pl>
2024-01-04 14:57:37 +0100

    [surpass] AutocorrelateVec3Measurements implemented
    AutocorrelateVec3Measurements is a new SystemMeasurement that measures autocorrelation of any system property that returns Vec3 type. It'll be used to autocorrelate R_end vectors

--- surpass/src/observers.rs
+++ surpass/src/observers.rs
@@ -34,6 +34,52 @@ impl<T: std::fmt::Display, M: SystemMeasurement<T>> RecordMeasurements<T, M> {
     }
 }
 
+pub struct AutocorrelateVec3Measurements<M: SystemMeasurement<Vec3>> {
+    measurement: M,
+    i_chain: usize,
+    correlations: Vec<f64>,
+    observations: VecDeque<Vec3>,
+    fname: String,
+    n_samples: usize
+}
+
+impl<M: SystemMeasurement<Vec3>> AutocorrelateVec3Measurements<M> {
+
+    pub fn new(measurement: M, t_max: usize, file_name: &str) -> AutocorrelateVec3Measurements<M> {
+        return AutocorrelateVec3Measurements{
+            measurement, i_chain: 0,
+            correlations: vec![0.0; t_max],
+            observations: Default::default(),
+            fname: file_name.to_string(), n_samples: 0
+        };
+    }
+
+    pub fn observe(&mut self, system: &SurpassAlphaSystem) {
+        // --- the number of points to compute product with
+        let t_max = self.correlations.len();
+        // --- take the measurement of the current conformation
+        let v = self.measurement.measure(system);
+        if self.n_samples < self.correlations.len() {
+            self.observations.push_front(v);
+        } else {
+            // --- compute products, update statistics
+            for i_time in 0..t_max {
+                self.correlations[i_time] += Vec3::dot(&self.observations[i_time], &v);
+            }
+            self.observations.pop_back();
+            self.observations.push_front(v);
+        }
+        self.n_samples += 1;
+    }
+
+    pub fn observed_values(&self) -> Vec<f64> {
+        self.correlations.iter().map(|vi| vi / self.n_samples as f64).collect()
+    }
+
+    pub fn n_observations(&self) -> usize { self.n_samples }
+}
+
+
 /// Measures a property of a [`SurpassAlphaSystem`](SurpassAlphaSystem)
 ///
 /// At each call of  the [`SystemMeasurement::measure()`](SystemMeasurement::measure()) method,
---
+++ surpass/tests/test_autocorrelations.rs
@@ -0,0 +1,46 @@
+use rand::Rng;
+use bioshell_pdb::calc::Vec3;
+use surpass::{AutocorrelateVec3Measurements, SurpassAlphaSystem, SystemMeasurement};
+
+struct Vec3Measurement { last_vec: Vec3 }
+
+impl SystemMeasurement<Vec3> for Vec3Measurement {
+
+    fn measure(&self, system: &SurpassAlphaSystem) -> Vec3 { return system.ca_to_vec3(0) }
+
+    fn header(&self) -> String { unimplemented!() }
+}
+
+fn step(step_size: f64, system: &mut SurpassAlphaSystem) {
+    let mut v = system.ca_to_vec3(0);
+    let mut rng = rand::thread_rng();
+    v.x += rng.gen::<f64>() * step_size - step_size/2.0;
+    v.y += rng.gen::<f64>() * step_size - step_size/2.0;
+    v.z += rng.gen::<f64>() * step_size - step_size/2.0;
+    v.normalize();
+    system.vec3_to_ca(0, &v);
+}
+
+
+#[test]
+fn test_autocorrelations() {
+    // --- the system is not used
+    let mut s = SurpassAlphaSystem::new(&[5], 100.0);
+    let m = Vec3Measurement{ last_vec: Default::default() };
+    let t_max = 100;
+    let n_samples = 100000;
+    let mut autocorr = AutocorrelateVec3Measurements::new(m, t_max, "out");
+    for i in 0..n_samples {
+        autocorr.observe(&s);
+        step(0.5, &mut s);
+    }
+
+    assert_eq!(autocorr.n_observations(), n_samples);
+    let obs = autocorr.observed_values();
+    assert_eq!(obs.len(), t_max);
+    assert!(obs[0] > 0.9);
+    assert!(obs[t_max - 1] < 0.5);
+    // for i in 0..t_max {
+    //     println!("{} {}", i+1, obs[i]);
+    // }
+}
