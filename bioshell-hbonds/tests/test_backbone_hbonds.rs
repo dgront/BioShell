7a6a6dc66afcb1489cab403b277d539a133fced1
Dominik Gront <dgront@chem.uw.edu.pl>
2024-12-04 20:31:45 +0100

    [bioshell-hbonds] unit test added

---
+++ bioshell-hbonds/tests/test_backbone_hbonds.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+    use bioshell_hbonds::BackboneHBondMap;
+    use bioshell_pdb::Deposit;
+
+    #[allow(non_upper_case_globals)]
+    const cif_2fdo:  &str = include_str!("./test_files/2fdo.cif");
+
+    #[test]
+    fn hbonds_2fdo() {
+
+        let reader = BufReader::new(cif_2fdo.as_bytes());
+        let strctr = Deposit::from_cif_reader(reader).unwrap().structure();
+        let hbonds = BackboneHBondMap::new(&strctr);
+    }
+}
