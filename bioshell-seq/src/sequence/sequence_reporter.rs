b29ae01fe244599347722d9f3dfd3018f9fec02a
Dominik Gront <dgront@chem.uw.edu.pl>
2025-03-25 23:02:17 +0100

    [bioshell-seq] documentation for WriteFasta and SplitFasta structs

--- bioshell-seq/src/sequence/sequence_reporter.rs
+++ bioshell-seq/src/sequence/sequence_reporter.rs
@@ -5,17 +5,26 @@ use bioshell_io::out_writer;
 use crate::sequence::{first_word_of_description, Sequence};
 
 /// A trait for types that can report on a `Sequence`, e.g. write it to a file.
+///
 pub trait SequenceReporter {
     /// Reports on the provided `Sequence`.
     fn report(&mut self, seq: &Sequence) -> io::Result<()>;
 }
 
+/// A [`SequenceReporter`](SequenceReporter) that appends a [`Sequence`](Sequence) to a file in FASTA format.
+///
+/// All the sequences reported by this struct are written to the same file.
 pub struct WriteFasta {
     line_width: usize,
     writer: Box<dyn Write>
 }
 
 impl WriteFasta {
+    /// Creates a new [`WriteFasta`](WriteFasta) object.
+    ///
+    /// If `file_name` is `None`, or `"stdout"` the output is written to stdout. Otherwise, a new file is created.
+    /// If `if_append` is `true`, the output is appended to the file if it exists.
+    /// Sequence is wrapped every `line_width` characters.
     pub fn new(file_name: Option<String>, line_width: usize, if_append: bool) -> Self {
         let out = match &file_name {
             None => out_writer("", true),
@@ -31,12 +40,19 @@ impl SequenceReporter for WriteFasta {
     }
 }
 
+/// A [`SequenceReporter`](SequenceReporter) that writes each [`Sequence`](Sequence) to a *separate* file in FASTA format.
+///
+/// Each sequence is written to a separate file, named after the first word of its description.
 pub struct SplitFasta {
     line_width: usize,
     path: Option<String>,
 }
 
 impl SplitFasta {
+    /// Creates a new [`SplitFasta`](SplitFasta) object.
+    ///
+    /// The files are written to a directory specified by `path`. If `path` is `None`, the files are written to the current directory.
+    /// Sequence is wrapped every `line_width` characters.
     pub fn new(path: Option<String>, line_width: usize) -> Self {
         Self { line_width, path }
     }
