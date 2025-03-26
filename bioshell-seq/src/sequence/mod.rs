7502834580570003857e3daae8c0612f743ccd69
Dominik Gront <dgront@chem.uw.edu.pl>
2025-03-26 08:06:39 +0100

    [bioshell-seq] documentation and example for first_word_of_description()

--- bioshell-seq/src/sequence/mod.rs
+++ bioshell-seq/src/sequence/mod.rs
@@ -21,7 +21,7 @@ use crate::SequenceError;
 ///
 /// If the input string contains a dot character, it's assumed to be a file name. This function
 /// attempts to open that file as FASTA and load all sequences stored.
-/// Otherwise it's assumed the string is an amino acid sequence by itself; it's converted into sequence and returned
+/// Otherwise, it's assumed the string is an amino acid sequence by itself; it's converted into sequence and returned
 /// as the only element of a vector.
 pub fn load_sequences(seq_or_fname: &String, seq_name: &str) -> Result<Vec<Sequence>, SequenceError> {
     if seq_or_fname.contains(".") {
@@ -36,6 +36,19 @@ pub fn load_sequences(seq_or_fname: &String, seq_name: &str) -> Result<Vec<Seque
 }
 
 /// Returns the first word of a sequence description string.
+///
+/// This function is used to extract the ID of a sequence from it's description, assuming it's the first word
+///
+/// # Examples
+/// ```
+/// use bioshell_seq::sequence::first_word_of_description;
+/// let description = "1ABC A hypothetical protein from E. coli";
+/// let id = first_word_of_description(description);
+/// assert_eq!(id, "1ABC");
+/// let description = "NC_000913.3 Escherichia coli str. K-12 substr. MG1655, complete genome";
+/// let id = first_word_of_description(description);
+/// assert_eq!(id, "NC_000913.3");
+/// ```
 pub fn first_word_of_description(description: &str) -> String {
     let v: Vec<&str> = description.split_whitespace().collect();
 
