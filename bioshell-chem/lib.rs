8235e2b13d24e21c465a3294f07467d22c477671
Dominik Gront <dgront@chem.uw.edu.pl>
2026-05-13 12:25:53 +0200

    [chem] Molecule struct started

--- bioshell-chem/lib.rs
+++
@@ -1,3 +0,0 @@
-/// Library for chemical informatics in Rust.
-///
-/// This library provides tools for working with chemical structures,
---
+++ bioshell-chem/src/lib.rs
@@ -0,0 +1,6 @@
+//! Library for chemical informatics in Rust.
+//!
+//! This library provides tools for working with chemical structures.
+
+mod molecule;
+pub use molecule::{Molecule};
