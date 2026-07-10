dd66c979c355c675b3676f514249293c95d6bfec
Dominik Gront <dgront@chem.uw.edu.pl>
2026-07-10 14:22:10 +0200

    [pyo3,seq] FastaIterator uses parsing_mode
    The recent changes in bioshell-seq now available in Python:

--- pybioshell/docs/seq/fasta_iterator.rst
+++ pybioshell/docs/seq/fasta_iterator.rst
@@ -1,7 +1,12 @@
 An iterator over sequences in a FASTA file.
 
 Yields `Sequence` objects one by one; reading only a fragment
-of the input file at a time.
+of the input file at a time. The sequences are parsed with one of the following ``parsing_mode`` strategies:
+
+- ``"raw"`` - as is, without any quality control
+- ``"protein"`` - characters other than denoting amino acids will be removed
+- ``"protein*"`` - as in  ``"protein"`` but stop codon denoted as ``'*'`` is allowed in a sequence
+- ``"nucleic"`` - characters other than denoting nucleic acids will be removed
 
 Example
 --------
@@ -11,7 +16,7 @@ Example
     from bioshell.seq import FastaIterator
 
     n_seq = 0
-    for seq in FastaIterator("test_files/fdx_2fe_2s.fasta"):
+    for seq in FastaIterator("test_files/fdx_2fe_2s.fasta", "protein"):
         print(seq.id)
         n_seq += 1
     print(f"Processed {n_seq} sequences")
--- pybioshell/src/seq/py_fasta_iterator.rs
+++ pybioshell/src/seq/py_fasta_iterator.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::{BufReader};
 use pyo3::prelude::*;
-use pyo3::exceptions::PyStopIteration;
-use bioshell_seq::sequence::FastaIterator;
+use pyo3::exceptions::{PyStopIteration, PyValueError};
+use bioshell_seq::sequence::{FastaIterator, FastaParsingMode};
 use crate::seq::PySequence;
 
 #[doc = include_str!("../../docs/seq/fasta_iterator.rst")]
@@ -11,21 +11,39 @@ pub struct PyFastaIterator {
     inner: FastaIterator<BufReader<File>>,
 }
 
+fn parsing_mode_from_str(parsing_mode: &str) -> Result<FastaParsingMode, PyErr> {
+
+    match parsing_mode.trim().to_ascii_lowercase().as_str() {
+        "raw" | "none" => Ok(FastaParsingMode::Raw),
+        "protein" | "clean-protein" | "clean_protein" => Ok(FastaParsingMode::CleanProtein),
+        "nucleic" | "clean-nucleic" | "clean_nucleic" => Ok(FastaParsingMode::CleanNucleic),
+        "protein*" | "clean-protein*" | "clean_protein*"| "clean-protein-stop" | "clean_protein_stop" => Ok(FastaParsingMode::CleanProteinStop),
+
+        _ => Err(PyValueError::new_err(format!(
+            "invalid parsing_mode {parsing_mode:?}; expected one of: 'raw', 'protein', 'protein*', nucleic"
+        ))),
+    }
+}
+
+
 #[pymethods]
 impl PyFastaIterator {
-    /// Create a new iterator from a FASTA file path.
+    /// Create a new iterator from a FASTA file `path`.
     ///
-    /// Args:
-    ///     path (str): Path to the FASTA file.
+    /// Depending on the ``parsing_mode``, the file will be parsed:
+    ///   - `"raw"` - as is, without any quality control
+    ///   - `"protein"` - characters other than denoting amino acids will be removed
+    ///   - `"protein*"` - as in  `"protein"` but stop codon denoted as `*` is allowed
+    ///   - `"nucleic"` - characters other than denoting nucleic acids will be removed
     ///
     /// Returns:
     ///     FastaIterator: An iterator over sequences.
     #[new]
-    pub fn new(path: &str) -> PyResult<Self> {
+    pub fn new(path: &str, parsing_mode: &str) -> PyResult<Self> {
         let file = File::open(path).map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Failed to open file: {}", e)))?;
         let reader = BufReader::new(file);
         Ok(PyFastaIterator {
-            inner: FastaIterator::new(reader),
+            inner: FastaIterator::new(reader, parsing_mode_from_str(parsing_mode)?),
         })
     }
 
@@ -39,7 +57,7 @@ impl PyFastaIterator {
         match self.inner.next() {
             Some(result) => result
                 .map(|seq| PySequence { inner: seq })
-                .map_err(|err| pyo3::exceptions::PyValueError::new_err(err.to_string())),
+                .map_err(|err| PyValueError::new_err(err.to_string())),
             None => Err(PyStopIteration::new_err("End of FASTA file")),
         }
     }
